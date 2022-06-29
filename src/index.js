19ada7fbee7c41a773d33c12b2cfe6924356b572
baIder <511763880@qq.com>
2022-06-29 16:14:52 +0800

    remove image

--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import x from "./main.js";
 import png from "./assets/1.png";
 console.log(x);
 console.log(png);
-const div = document.getElementById("app");
-console.log(div);
-div.innerHTML = `
-    <img src="${png}">
-`;
+
+// const div = document.getElementById("app");
+// console.log(div);
+// div.innerHTML = `
+//     <img src="${png}">
+// `;
 
 const button = document.createElement("button");
 button.innerText = "懒加载";
